c80aae41a401d802c22b2f138d43fa069f26673d
BachLong-29 <65652138+BachLong-29@users.noreply.github.com>
2022-02-10 13:53:36 +0700

    Add files via upload

---
+++ application/views/_shared/css.php
@@ -0,0 +1,13 @@
+
+
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" href="./public/assets/img/Capture.PNG"  type="image/png" sizes="16x16 32x32">
+
+<link rel="stylesheet" href="./public/assets/css/user.css"/>
+<link rel="stylesheet" href="./public/assets/css/admin.css"/>
+<link rel="stylesheet" href="./public/assets/DataTables/datatables.css"/>
+<link rel="stylesheet" href="./public/assets/fontawesome-free-5.15.1-web/css/all.css"/>
+<link rel="stylesheet" href="./public/assets/bootstrap-4.5.3-dist/css/bootstrap.min.css"/>
---
+++ application/views/_shared/js.php
@@ -0,0 +1,7 @@
+<!-- <script src="<?php echo BASEURL?>/public/assets/js/config.js"></script> -->
+<script src="./public/assets/bootstrap-4.5.3-dist/js/bootstrap.bundle.min.js"></script>
+<script src="./public/assets/js/my-script.js"></script>
+<script src="./public/assets/DataTables/datatables.js"></script>
+<script src="./public/assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="./public/js/my-script.js"></script>
